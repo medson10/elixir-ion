67ed2415722c6fae01a68aada1e9e651b0f26f08
Raphael Thomazella <thomazella9@gmail.com>
2020-03-04 22:07:10 -0300

    chore(repo): init

---
+++ lib/elixirIon.ex
@@ -0,0 +1,18 @@
+defmodule ElixirIon do
+  @moduledoc """
+  Documentation for ElixirIon.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirIon.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ion_test.exs
@@ -0,0 +1,8 @@
+defmodule IonTest do
+  use ExUnit.Case
+  doctest ElixirIon
+
+  test "greets the world" do
+    assert ElixirIon.hello() == :world
+  end
+end
